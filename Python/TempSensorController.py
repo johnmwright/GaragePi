b142df63af42dc1f63de734d6d8cbf8470b592f9
John M. Wright <john@wrightfully.com>
2015-05-17 19:09:54 -0500

    adding time for mods to load

--- Python/TempSensorController.py
+++ Python/TempSensorController.py
@@ -14,6 +14,7 @@ class TempSensorController:
 
         os.system('modprobe w1-gpio')
         os.system('modprobe w1-therm')
+        time.sleep(2)
 
         base_dir = '/sys/bus/w1/devices/'
         device_folder = glob.glob(base_dir + '28*')[self.sensorIndex]
